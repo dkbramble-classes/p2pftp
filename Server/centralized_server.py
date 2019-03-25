5c8656e5e9a0b8e80ad2be1910af3127b356324a
BrotherB1 <bassettl@mail.gvsu.edu>
2019-03-24 20:19:37 -0400

    Completed Search function in Server

--- Server/centralized_server.py
+++ Server/centralized_server.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from io import BytesIO
 from socketserver import ThreadingMixIn
 import json
-import time
+from urllib.parse import urlparse
 # https://blog.anvileight.com/posts/simple-python-http-server/
 # https://gist.github.com/bradmontgomery/2219997
 # curl -d "username_hostname_connection" http://localhost
@@ -14,6 +14,33 @@ class Database(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+
+    def do_GET(self):
+        #this is the function for the search command. Dane will use requests.get(url = URL, params = PARAMS) to request information
+        query = urlparse(self.path).query
+        print(query)
+        searchStr = str(query)
+        search = searchStr.split("=")
+        x = 0
+        response = {}
+        print(search[1])
+        for username in userFiles:
+            for file in username:
+                if search[1] in userFiles[username][file]:
+                    response[x] = {
+                        "hostname": userInfo[username]["hostname"],
+                        "connection": userInfo[username]["connection"],
+                        "file": file,
+                        "description": userFiles[username][file]
+                    }
+                    x += 1
+        self.send_response(200)
+        self.end_headers()
+        response = "Get Gotten"
+        self.wfile.write(response.encode("utf-8"))
+
+        #query_components = dict(qc.split("=") for qc in query.split("&"))
+
         
     def do_POST(self):
         # Doesn't do anything with posted data
@@ -22,25 +49,38 @@ class Database(BaseHTTPRequestHandler):
         strBody = str(body, 'utf-8')
         parsedBody = strBody.split("_")
         # If the information is about a user, store the username with the hostname and connection as a smaller dictionary
+        # This worked: curl -d "User_username2_hostname_connection" http://Lukes-MacBook-Pro-2.local
         if parsedBody[0] == "User":
             userInfo[parsedBody[1]] = {
                 "hostname": parsedBody[2],
                 "connection": parsedBody[3]
             }
+            response = "The User storage has been sucessful!!!!!!"
+            self.wfile.write(response.encode("utf-8"))
         #Otherwise if the information is for a file, store the file name and descritpion as a list of dictionaries under the username
+        #The JSON text that we send here to the server needs to have escape characters 
+        # This worked: curl -d "File_Dane_{\"local_server.py\":\"Insert Description Here\",\"ftp_client.py\":\"Look, More Descriptions\",\"client_gui.py\":\"DESCRIPTIONS\" }" http://Lukes-MacBook-Pro-2.local
         elif parsedBody[0] == "File":
-            datastore = json.loads(parsedBody[2])
+            jsonString = parsedBody[2]
+            for x in range(3, len(parsedBody)):
+                jsonString += "_" + str(parsedBody[x])
+            print(jsonString)
+            datastore = json.loads(jsonString)
+            if parsedBody[1] not in userFiles:
+                userFiles[parsedBody[1]] = {}
             for file in datastore:
+                print(file)
                 userFiles[parsedBody[1]][file] =  datastore[file]
+            response = "The File Description storage has been sucessful!!!!!!"
+            self.wfile.write(response.encode("utf-8"))
+        #Return information as JSON payload 
         elif parsedBody[0] == "Quit":
             userInfo.delete(parsedBody[1])
             userFiles.delete(parsedBody[2])
         else:
             print("Didn't receive proper request")
         print("This is the body: " + strBody)
-        print("This is UserInfo: " )
-        for user in userInfo:
-            print(user)
+        #print("This is a file check: " + userFiles["Dane"]["local_server.py"] )
         self.send_response(200)
         self.end_headers()
 
