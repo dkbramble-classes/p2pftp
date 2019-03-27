107dbbeac22ac540068ba3b0f9825da0af9bbaa6
Brendan Warnick <warnickb@eos15.cis.gvsu.edu>
2019-03-27 18:43:10 -0400

    centralized server multithreading

--- Server/centralized_server.py
+++ Server/centralized_server.py
@@ -7,6 +7,9 @@ from urllib.parse import urlparse
 # https://gist.github.com/bradmontgomery/2219997
 # curl -d "username_hostname_connection" http://localhost
 
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
+
 userInfo = {}
 userFiles = {}
 class Database(BaseHTTPRequestHandler):
@@ -92,8 +95,8 @@ class Database(BaseHTTPRequestHandler):
         # self.wfile.write(response.getvalue())
         
 def run(server_class=HTTPServer, handler_class=Database, port=80):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
+    server_address = '0.0.0.0'
+    httpd = ThreadingSimpleServer((server_address, port), handler_class)
     httpd.serve_forever()
 
 if __name__ == "__main__":
